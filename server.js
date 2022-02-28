540cea4c56a5d241f00af727d50a855557150f63
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-02-28 13:26:36 -0500

    cola/issue #6: create a homepage

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.engine('view engine', 'handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on routes
 app.use(routes);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
