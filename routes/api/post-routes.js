56d634632781ffde3b61741017da47b39d4e4036
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-03-14 01:56:36 -0400

    cola/issue #5: created comment model, comment routes, and updated GET all post and user requests to include comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
-const { resolveSoa } = require('dns');
-const { rmSync } = require('fs');
 
 // get all users
 router.get('/', (req, res) => {
-  console.log('======================');
-
   Post.findAll({
     // query config
+    order: [['created_at', 'DESC']],
     attributes: [
       'id',
       'post_url',
@@ -17,8 +14,15 @@ router.get('/', (req, res) => {
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [['created_at', 'DESC']],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       { 
         model: User,
         attributes: ['username']
